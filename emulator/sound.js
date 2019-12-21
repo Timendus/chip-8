3dc2bfb1a454ca3d9151d9ae67b260463d9cc606
Tim Franssen <tim.franssen@nedap.com>
2019-12-22 00:28:27 +0100

    Make the beep code a bit more elegant

--- emulator/sound.js
+++ emulator/sound.js
@@ -1,37 +1,28 @@
 module.exports = {
   connect: state => {
 
-    // Some beep code I pulled off Stack Overflow ;)
+    // Set up audio stuff to sound a nasty beep
 
-    //if you have another AudioContext class use that one, as some browsers have a limit
-    var audioCtx = new (window.AudioContext || window.webkitAudioContext || window.audioContext);
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext || window.audioContext);
 
-    //All arguments are optional:
+    const oscillator = audioCtx.createOscillator();
+    const gainNode = audioCtx.createGain();
 
-    //duration of the tone in milliseconds. Default is 500
-    //frequency of the tone in hertz. default is 440
-    //volume of the tone. Default is 1, off is 0.
-    //type of tone. Possible values are sine, square, sawtooth, triangle, and custom. Default is sine.
-    //callback to use on end of tone
-    function beep(duration, frequency, volume, type, callback) {
-        var oscillator = audioCtx.createOscillator();
-        var gainNode = audioCtx.createGain();
+    gainNode.gain.value = 0;
+    oscillator.frequency.value = 600;
+    oscillator.type = 'triangle';
 
-        oscillator.connect(gainNode);
-        gainNode.connect(audioCtx.destination);
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
 
-        if (volume){gainNode.gain.value = volume;};
-        if (frequency){oscillator.frequency.value = frequency;}
-        if (type){oscillator.type = type;}
-        if (callback){oscillator.onended = callback;}
+    oscillator.start();
 
-        oscillator.start();
-        setTimeout(function(){oscillator.stop()}, (duration ? duration : 500));
-    };
+    // Check if we need to start or stop the beep
 
-    // If the sound timer is not zero yet, play a beep
     setInterval(() => {
-      if ( state.sound > 0 ) beep(17);
+      if ( state.sound >  0 ) gainNode.gain.value = 0.4;
+      if ( state.sound == 0 ) gainNode.gain.value = 0;
     }, 17) // 60Hz
+
   }
 }
