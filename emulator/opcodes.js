0f1b26da435c3aafc89ee7075145e2ebd8cf2e12
Tim Franssen <tim.franssen@nedap.com>
2019-12-23 17:37:54 +0100

    Rename opcodes (inspired by mattmik website, lowercase looks better imho). Also, fix a few bugs, mainly with F register.

--- emulator/opcodes.js
+++ emulator/opcodes.js
@@ -1,3 +1,8 @@
+// Opcodes as documented at:
+//   * https://en.wikipedia.org/wiki/CHIP-8
+//   * http://mattmik.com/files/chip8/mastering/chip8.html
+//   * http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+
 const s = require('./binary_strings');
 const keyboard = require('./keyboard');
 
@@ -6,23 +11,26 @@ module.exports = {
   '00E0': ({state}) => {
     for ( let n = 0x0F00; n <= 0x0FFF; n++ ) state.ram[n] = 0;
     state.sd = true;
-    return 'CLEAR';
+    return 'cls';
   },
 
   '00EE': ({state, disassemble}) => {
     if ( disassemble )
-      return 'RETURN';
+      return 'ret';
 
     state.sp += 2;
     state.pc = state.ram[state.sp - 0] * 0x100 + state.ram[state.sp - 1];
-    return `RETURN ${s.word2str(state.pc)}`;
+    return `ret (to ${s.word2str(state.pc)})`;
   },
 
-  '0...': () => console.error('RCA 1802 Not implemented'),
+  '0...': ({state, nnn}) => {
+    console.error('RCA 1802 Not implemented');
+    return `rca 1802 call ${s.word2str(nnn)}`;
+  },
 
   '1...': ({state, nnn}) => {
     state.pc = nnn - 2; // Step back one instruction because we step ahead later
-    return `JUMP ${s.word2str(nnn)}`;
+    return `jp ${s.word2str(nnn)}`;
   },
 
   '2...': ({state, nnn}) => {
@@ -30,191 +38,211 @@ module.exports = {
     state.ram[state.sp - 1] =  state.pc & 0x00ff;
     state.sp -= 2;
     state.pc = nnn - 2; // Step back one instruction because we step ahead later
-    return `CALL ${s.word2str(nnn)}`;
+    return `call ${s.word2str(nnn)}`;
   },
 
   '3...': ({state, x, nn}) => {
     if ( state.v[x] === nn ) state.pc += 2;
-    return `SKIP_IF_EQUAL_TO V${x}, ${s.byte2str(nn)}`;
+    return `se v${x}, ${s.byte2str(nn)}`;
   },
 
   '4...': ({state, x, nn}) => {
     if ( state.v[x] !== nn ) state.pc += 2;
-    return `SKIP_IF_UNEQUAL_TO V${x}, ${s.byte2str(nn)}`;
+    return `sne v${x}, ${s.byte2str(nn)}`;
   },
 
   '5..0': ({state, x, y}) => {
     if ( state.v[x] === state.v[y] ) state.pc += 2;
-    return `SKIP_IF_SAME V${x}, V${y}`;
+    return `se v${x}, v${y}`;
   },
 
   '6...': ({state, x, nn}) => {
     state.v[x] = nn;
-    return `LOAD V${x}, ${s.byte2str(nn)}`;
+    return `ld v${x}, ${s.byte2str(nn)}`;
   },
 
   '7...': ({state, x, nn}) => {
     state.v[x] += nn;
-    return `ADD_VALUE V${x}, ${s.byte2str(nn)}`;
+    return `add v${x}, ${s.byte2str(nn)}`;
   },
 
   '8..0': ({state, x, y}) => {
     state.v[x] = state.v[y];
-    return `ASSIGN V${x}, V${y}`;
+    return `ld v${x}, v${y}`;
   },
 
   '8..1': ({state, x, y}) => {
     state.v[x] |= state.v[y];
-    return `OR V${x}, V${y}`;
+    return `or v${x}, v${y}`;
   },
 
   '8..2': ({state, x, y}) => {
     state.v[x] &= state.v[y];
-    return `AND V${x}, V${y}`;
+    return `and v${x}, v${y}`;
   },
 
   '8..3': ({state, x, y}) => {
     state.v[x] ^= state.v[y];
-    return `XOR V${x}, V${y}`;
+    return `xor v${x}, v${y}`;
   },
 
   '8..4': ({state, x, y}) => {
-    state.v[x] += state.v[y];
-    state.v[0xF] = state.v[x] + state.v[y] > 255 ? 1 : 0;
-    return `ADD V${x}, V${y}`;
+    // Set VF to 01 if a carry occurs
+    // Set VF to 00 if a carry does not occur
+    state.v[0xF] = (state.v[x] + state.v[y]) > 0xFF ? 1 : 0;
+    state.v[x]  += state.v[y];
+    return `add v${x}, v${y}`;
   },
 
   '8..5': ({state, x, y}) => {
-    state.v[x] -= state.v[y];
-    state.v[0xF] = state.v[x] >= state.v[y] ? 1 : 0;
-    return `SUB V${x}, V${y}`;
+    // Set VF to 00 if a borrow occurs
+    // Set VF to 01 if a borrow does not occur
+    state.v[0xF] = state.v[y] > state.v[x] ? 0 : 1;
+    state.v[x]  -= state.v[y];
+    return `sub v${x}, v${y}`;
   },
 
   '8..6': ({state, x}) => {
+    // Set register VF to the least significant bit prior to the shift
     state.v[0xF] = state.v[x] & 0b00000001 ? 1 : 0;
     state.v[x]   = state.v[x] >> 1;
-    return `SHIFT_RIGHT V${x}`;
+    return `shr v${x}`;
   },
 
   '8..7': ({state, x, y}) => {
-    state.v[x] = state.v[y] - state.v[x];
-    state.v[0xF] = state.v[y] >= state.v[x] ? 1 : 0;
-    return `SUB_INVERSE V${x}, V${y}`;
+    // Set VF to 00 if a borrow occurs
+    // Set VF to 01 if a borrow does not occur
+    state.v[0xF] = state.v[x] > state.v[y] ? 0 : 1;
+    state.v[x]   = state.v[y] - state.v[x];
+    return `subn v${x}, v${y}`;
   },
 
   '8..E': ({state, x}) => {
+    // Set register VF to the most significant bit prior to the shift
     state.v[0xF] = state.v[x] & 0b10000000 ? 1 : 0;
     state.v[x]   = state.v[x] << 1;
-    return `SHIFT_LEFT V${x}`;
+    return `shl v${x}`;
   },
 
   '9..0': ({state, x, y}) => {
     if ( state.v[x] !== state.v[y] ) state.pc += 2;
-    return `SKIP_IF_UNEQUAL V${x}, V${y}`;
+    return `sne v${x}, v${y}`;
   },
 
   'A...': ({state, nnn}) => {
     state.i = nnn;
-    return `SET_I ${s.word2str(nnn)}`;
+    return `ld i, ${s.word2str(nnn)}`;
   },
 
   'B...': ({state, nnn}) => {
     state.pc = nnn + state.v[0] - 2; // Step back one instruction because we step ahead later
-    return `JUMP V0 + ${s.word2str(nnn)}`;
+    return `jp v0, ${s.word2str(nnn)}`;
   },
 
   'C...': ({state, x, nn}) => {
-    state.v[x] = Math.floor(Math.random() * 255) & nn;
-    return `RAND V${x}, ${s.byte2str(nn)}`;
+    state.v[x] = Math.floor(Math.random() * 0x100) & nn;
+    return `rnd v${x}, ${s.byte2str(nn)}`;
   },
 
   'D...': ({state, x, y, n}) => {
+    // Set VF to 01 if any set pixels are changed to unset, and 00 otherwise
     const xPos = state.v[x];
     const yPos = state.v[y];
 
     const memoryOffset = Math.floor(yPos * 8 + xPos / 8);
-    let flipped = false;
+    let erases = false;
 
     for ( let i = 0; i < n; i++ ) {
       const spritePart = state.ram[state.i + i];
 
-      flipped = !!(state.ram[0x0F00 + memoryOffset + i * 8 + 0] & spritePart >> (xPos % 8)) ||
+      // If video ram AND sprite > 0, they share pixels. If they share pixels,
+      // this will lead to erases because of the XOR.
+      erases |= !!(state.ram[0x0F00 + memoryOffset + i * 8 + 0] & spritePart >> (xPos % 8)) ||
                 !!(state.ram[0x0F00 + memoryOffset + i * 8 + 1] & spritePart << (8 - (xPos % 8)) & 0xFF);
 
       state.ram[0x0F00 + memoryOffset + i * 8 + 0] ^= spritePart >> (xPos % 8);
       state.ram[0x0F00 + memoryOffset + i * 8 + 1] ^= spritePart << (8 - (xPos % 8)) & 0xFF;
     }
     state.sd = true;
-    state.v[0xF] = flipped ? 1 : 0;
+    state.v[0xF] = erases ? 1 : 0;
 
-    return `DRAW ${xPos}, ${yPos}, ${n}, ${s.word2str(state.i)}`;
+    return `drw ${xPos}, ${yPos}, ${n} (with ${s.word2str(state.i)} in i)`;
   },
 
   'E.9E': ({state, x}) => {
     if ( keyboard.pressed(state.v[x]) ) state.pc += 2;
-    return `SKIP_IF_KEYPRESS V${x}`;
+    return `skp v${x}`;
   },
 
   'E.A1': ({state, x}) => {
     if ( !keyboard.pressed(state.v[x]) ) state.pc += 2;
-    return `SKIP_UNLESS_KEYPRESS V${x}`;
+    return `sknp v${x}`;
   },
 
   'F.07': ({state, x}) => {
     state.v[x] = state.delay;
-    return `GET_DELAY V${x}`;
+    return `ld v${x}, dt`;
   },
 
   'F.0A': ({state, x, disassemble}) => {
     if ( disassemble )
-      return `GET_KEY V${x}`;
+      return `getkey v${x}`;
 
     return keyboard.waitPress()
                    .then(key => {
                      state.v[x] = key;
-                     return `GET_KEY V${x}`;
+                     return `getkey v${x}`;
                    });
   },
 
   'F.15': ({state, x}) => {
     state.delay = state.v[x];
-    return `SET_DELAY V${x}`;
+    return `ld dt, v${x}`;
   },
 
   'F.18': ({state, x}) => {
     state.sound = state.v[x];
-    return `SET_SOUND V${x}`;
+    return `ld st, v${x}`;
   },
 
   'F.1E': ({state, x}) => {
-    const newValue = state.i + state.v[x];
-    state.v[0xF] = newValue > 0xFFF ? 1 : 0;
-    state.i = newValue & 0xFFF;
-    return `ADD_TO_I V${x}`;
+    // Overflow stored in VF according to Wikipedia, not
+    // according to both other sources... :/
+    // state.v[0xF] = (state.i + state.v[x]) > 0xFFF ? 1 : 0;
+    state.i = (state.i + state.v[x]) & 0xFFF;
+    return `add i, v${x}`;
   },
 
   'F.29': ({state, x}) => {
+    // Font is stored in first part of memory,
+    // each character is 5 bytes, so:
     state.i = state.v[x] * 5;
-    return `LOAD_FONT V${x}`;
+    return `getfont v${x}`;
   },
 
   'F.33': ({state, x}) => {
-    state.ram[state.i + 0] = state.v[x] % 1000 / 100;
+    state.ram[state.i + 0] = state.v[x] / 100;
     state.ram[state.i + 1] = state.v[x] % 100 / 10;
     state.ram[state.i + 2] = state.v[x] % 10;
-    return `STORE_BCD V${x}`;
+    return `bcd (i), v${x}`;
   },
 
   'F.55': ({state, x}) => {
+    // I is set to I + X + 1 after operation according to mattmik, left
+    // untouched according to Wikipedia, devernay has no mention of i... :/
     for ( let n = 0; n <= x; n++ )
       state.ram[state.i + n] = state.v[n];
-    return `SAVE_REGISTERS V0 - V${x}`;
+    // state.i += x + 1;
+    return `ld (i), v0-v${x}`;
   },
 
   'F.65': ({state, x}) => {
+    // I is set to I + X + 1 after operation according to mattmik, left
+    // untouched according to Wikipedia, devernay has no mention of i... :/
     for ( let n = 0; n <= x; n++ )
       state.v[n] = state.ram[state.i + n];
-    return `LOAD_REGISTERS V0 - V${x}`;
+    // state.i += x + 1;
+    return `ld v0-v${x}, (i)`;
   }
 
 }
