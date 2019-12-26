f14e6b20b670cd43a5e0f80023e9e7a5e5cb820f
Tim Franssen <tim.franssen@nedap.com>
2019-12-26 01:31:52 +0100

    Add more opcodes

--- assembler/opcodes.js
+++ assembler/opcodes.js
@@ -1,6 +1,7 @@
 const e = require('./expressions');
 
 module.exports = [
+
   {
     size:        2,
     bytes:       '00E0',
@@ -14,6 +15,7 @@ module.exports = [
       state.sd = true;
     }
   },
+
   {
     size:        2,
     bytes:       '00EE',
@@ -26,6 +28,7 @@ module.exports = [
       state.pc = state.ram[state.sp - 0] * 0x100 + state.ram[state.sp - 1];
     }
   },
+
   {
     size:        2,
     bytes:       `1${e.nnn}`,
@@ -38,6 +41,7 @@ module.exports = [
       state.pc = nnn - 2;
     }
   },
+
   {
     size:        2,
     bytes:       `2${e.nnn}`,
@@ -54,6 +58,7 @@ module.exports = [
       return `call ${s.word2str(nnn)}`;
     }
   },
+
   {
     size:        2,
     bytes:       `6${e.x}${e.nn}`,
@@ -65,6 +70,7 @@ module.exports = [
       state.v[x] = nn;
     }
   },
+
   {
     size:        2,
     bytes:       `7${e.x}${e.nn}`,
@@ -76,6 +82,97 @@ module.exports = [
       state.v[x] += nn;
     }
   },
+
+  {
+    size:        2,
+    bytes:       `8${e.x}${e.x}0`,
+    instruction: `ld ${e.register},${e.register}$`,
+    assemble:    ([x, y]) => [0x80 | x & 0xF, (y & 0xF) * 0x10],
+    disassemble: ([x, y]) => `ld v${x}, v${y}`,
+
+    run: (state, [, x, y]) => {
+      state.v[x] = state.v[y];
+    }
+  },
+
+  {
+    size:        2,
+    bytes:       `8${e.x}${e.x}1`,
+    instruction: `or ${e.register},${e.register}$`,
+    assemble:    ([x, y]) => [0x80 | x & 0xF, (y & 0xF) * 0x10 | 0x1],
+    disassemble: ([x, y]) => `or v${x}, v${y}`,
+
+    run: (state, [, x, y]) => {
+      state.v[x] |= state.v[y];
+    }
+  },
+
+  {
+    size:        2,
+    bytes:       `8${e.x}${e.x}2`,
+    instruction: `and ${e.register},${e.register}$`,
+    assemble:    ([x, y]) => [0x80 | x & 0xF, (y & 0xF) * 0x10 | 0x2],
+    disassemble: ([x, y]) => `and v${x}, v${y}`,
+
+    run: (state, [, x, y]) => {
+      state.v[x] &= state.v[y];
+    }
+  },
+
+  {
+    size:        2,
+    bytes:       `8${e.x}${e.x}3`,
+    instruction: `xor ${e.register},${e.register}$`,
+    assemble:    ([x, y]) => [0x80 | x & 0xF, (y & 0xF) * 0x10 | 0x3],
+    disassemble: ([x, y]) => `xor v${x}, v${y}`,
+
+    run: (state, [, x, y]) => {
+      state.v[x] ^= state.v[y];
+    }
+  },
+
+  {
+    size:        2,
+    bytes:       `8${e.x}${e.x}4`,
+    instruction: `add ${e.register},${e.register}$`,
+    assemble:    ([x, y]) => [0x80 | x & 0xF, (y & 0xF) * 0x10 | 0x4],
+    disassemble: ([x, y]) => `add v${x}, v${y}`,
+
+    run: (state, [, x, y]) => {
+      // Set VF to 01 if a carry occurs
+      // Set VF to 00 if a carry does not occur
+      state.v[0xF] = (state.v[x] + state.v[y]) > 0xFF ? 1 : 0;
+      state.v[x]  += state.v[y];
+    }
+  },
+
+  {
+    size:        2,
+    bytes:       `8${e.x}${e.x}5`,
+    instruction: `sub ${e.register},${e.register}$`,
+    assemble:    ([x, y]) => [0x80 | x & 0xF, (y & 0xF) * 0x10 | 0x5],
+    disassemble: ([x, y]) => `sub v${x}, v${y}`,
+
+    run: (state, [, x, y]) => {
+      // Set VF to 00 if a borrow occurs
+      // Set VF to 01 if a borrow does not occur
+      state.v[0xF] = state.v[y] > state.v[x] ? 0 : 1;
+      state.v[x]  -= state.v[y];
+    }
+  },
+
+  {
+    size:        2,
+    bytes:       `A${e.nnn}`,
+    instruction: `ld i,\\s?(${e.value}|${e.label})$`,
+    assemble:    ([nnn]) => [0xA0 | (nnn & 0xF00) / 0x100, nnn & 0xFF],
+    disassemble: ([nnn]) => `ld i, ${nnn}`,
+
+    run: (state, [, nnn]) => {
+      state.i = nnn;
+    }
+  },
+
   {
     size:        2,
     bytes:       `D${e.x}${e.y}${e.n}`,
@@ -106,6 +203,7 @@ module.exports = [
       state.v[0xF] = erases ? 1 : 0;
     }
   },
+
   {
     size:        2,
     bytes:       `F${e.x}0A`,
@@ -121,6 +219,22 @@ module.exports = [
                      });
     }
   },
+
+  {
+    size:        2,
+    bytes:       `F${e.x}1E`,
+    instruction: `add i,${e.register}$`,
+    assemble:    ([x]) => [0xF0 | x & 0xF, 0x1E],
+    disassemble: ([x]) => `add i, v${x}`,
+
+    run: (state, [, x]) => {
+      // Overflow stored in VF according to Wikipedia, not
+      // according to both other sources... :/
+      // state.v[0xF] = (state.i + state.v[x]) > 0xFFF ? 1 : 0;
+      state.i = (state.i + state.v[x]) & 0xFFF;
+    }
+  },
+
   {
     size:        2,
     bytes:       `F${e.x}29`,
@@ -133,5 +247,36 @@ module.exports = [
       // each character is 5 bytes, so:
       state.i = state.v[x] * 5;
     }
+  },
+
+  {
+    size:        2,
+    bytes:       `F${e.x}55`,
+    instruction: `ld \\(i\\),${e.register}$`,
+    assemble:    ([x]) => [0xF0 | x & 0xF, 0x55],
+    disassemble: ([x]) => `ld (i), v${x}`,
+
+    run: (state, [, x]) => {
+      // I is set to I + X + 1 after operation according to mattmik, left
+      // untouched according to Wikipedia, devernay has no mention of i... :/
+      for ( let n = 0; n <= x; n++ )
+        state.ram[state.i + n] = state.v[n];
+    }
+  },
+
+  {
+    size:        2,
+    bytes:       `F${e.x}65`,
+    instruction: `ld ${e.register},\\s?\\(i\\)$`,
+    assemble:    ([x]) => [0xF0 | x & 0xF, 0x65],
+    disassemble: ([x]) => `ld v${x}, (i)`,
+
+    run: (state, [, x]) => {
+      // I is set to I + X + 1 after operation according to mattmik, left
+      // untouched according to Wikipedia, devernay has no mention of i... :/
+      for ( let n = 0; n <= x; n++ )
+        state.v[n] = state.ram[state.i + n];
+    }
   }
+
 ]
